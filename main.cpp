7b8b91a8f7a42af979abc3c3549a97b13cb0fb5a
imr <ian@hot-rain.com>
2011-06-26 22:30:15 -0400

    Initial project start

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ian
+ *
+ * Created on June 26, 2011, 10:23 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
